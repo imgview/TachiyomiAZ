ouassim-boulkamh/amatsubu

Add Browse to start screen options

app/src/main/java/eu/kanade/tachiyomi/ui/main/MainActivity.kt
@@ -53,6 +53,7 @@ class MainActivity : BaseActivity<MainActivityBinding>() {
         when (preferences.startScreen()) {
             2 -> R.id.nav_history
             3 -> R.id.nav_updates
+            4 -> R.id.nav_browse
             else -> R.id.nav_library
         }
     }

imgview/TachiyomiAZ

(cherry picked from commit 3e837f8781f5e0237ed5bdcf771f4f2e64097991)

app/src/main/java/eu/kanade/tachiyomi/ui/main/MainActivity.kt
@@ -83,6 +83,7 @@ class MainActivity : BaseActivity<MainActivityBinding>() {
         when (preferences.startScreen()) {
             2 -> R.id.nav_drawer_history
             3 -> R.id.nav_drawer_updates
+            4 -> R.id.nav_drawer_sources
             else -> R.id.nav_drawer_library
         }
     }